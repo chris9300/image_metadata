1a40cddc72b28c7679dd50147fa07313caff25c4
Christian Henke <christian.henke@med.uni-goettingen.de>
2018-10-17 15:09:52 +0100

    Add example with Metadata extractor

--- src/main/java/ImageLibExamples.java
+++ src/main/java/ImageLibExamples.java
@@ -6,31 +6,89 @@ import javax.imageio.ImageIO;
 
 import org.apache.commons.imaging.*;
 import org.apache.commons.imaging.common.ImageMetadata;
+import org.apache.commons.imaging.formats.jpeg.JpegImageMetadata;
+import org.apache.commons.imaging.formats.tiff.TiffField;
+import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
+import org.apache.commons.imaging.formats.tiff.constants.TiffConstants;
+import org.apache.commons.imaging.formats.tiff.taginfos.TagInfo;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
 
 public class ImageLibExamples {
-	final String testFilePath = "src\\main\\resources\\test_img1.jpg";
+	final String testFilePath = "src/main/resources/test_img1.jpg";
 
-	public void loadImage() {
+	public void apacheCommonsImageExample() {
 		try {
+			
+			if (System.getProperty("os.name").startsWith("Windows")) {
+				testFilePath.replace("/", "\\");
+			}
+				
+			// 
+			//File.separator
 
 			File imgFile = new File(testFilePath);
 			BufferedImage img = ImageIO.read(imgFile);
 			System.out.println("image file readed successfully");
-			
+
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			ImageIO.write(img, "jpg", baos);
 			byte[] imageBytes = baos.toByteArray();
-			
+
 			ImageMetadata metadata = Imaging.getMetadata(imgFile);
-			
-			if(metadata != null) {
-			  System.out.println("Meta:" + metadata);
+
+			if (metadata != null) {
+				if (metadata instanceof JpegImageMetadata) {
+					JpegImageMetadata jpegMetadata = (JpegImageMetadata) metadata;
+					System.out.println("----------- JPEG Metadata -------");
+					printTagValue(jpegMetadata, TiffConstants.TIFF_TAG_DATE_TIME);
+					printTagValue(jpegMetadata, ExifTagConstants.EXIF_TAG_EXIF_IMAGE_WIDTH);
+					printTagValue(jpegMetadata, TiffConstants.EXIF_TAG_DATE_TIME_ORIGINAL);
+				}
+
+				System.out.println("Meta:" + metadata);
 			} else {
 				System.out.println("No Metadata available");
 			}
-			
+
 		} catch (final Exception e) {
-			System.out.println("Cannot read image file:" + e.getMessage());
+			System.out.println("Cannot read image file: " + e.getMessage());
+		}
+	}
+	
+
+	private static void printTagValue(final JpegImageMetadata jpegMetadata, final TagInfo tagInfo) {
+		final TiffField field = jpegMetadata.findEXIFValueWithExactMatch(tagInfo);
+		if (field == null) {
+			System.out.println(tagInfo.name + ": " + "Not Found.");
+		} else {
+			System.out.println(tagInfo.name + ": " + field.getValueDescription());
+		}
+	}
+	
+	public void metadataExtractorExample() {
+		File imgFile = new File(testFilePath);
+		try {
+		Metadata metadata = ImageMetadataReader.readMetadata(imgFile);
+		
+		for (Directory directory : metadata.getDirectories()) {
+		    for (Tag tag : directory.getTags()) {
+		        System.out.format("[%s] - %s = %s",
+		            directory.getName(), tag.getTagName(), tag.getDescription());
+		        System.out.println("");
+		    }
+		    if (directory.hasErrors()) {
+		        for (String error : directory.getErrors()) {
+		            System.err.format("ERROR: %s", error);
+		        }
+		    }
+		}
+		
+		} catch(Exception e) {
+			System.out.println("Error with Metadata Extractor: " + e.getMessage());
 		}
 	}
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Run started...");
+		System.out.println(System.getProperty("os.name"));
 		ImageLibExamples imgLibExample = new ImageLibExamples();
-		imgLibExample.loadImage();
+		System.out.println("----------- Apache Commons Imaging -------------");
+		imgLibExample.apacheCommonsImageExample();
 		
+		System.out.println("----------- Metadata Extractor -------------");
+		imgLibExample.metadataExtractorExample();
 	}
 
 }
