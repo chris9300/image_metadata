0dcac27cfb16810127d2f6b78cebf39c49e0d2b8
Christian Henke <christian.henke@med.uni-goettingen.de>
2018-10-26 18:28:35 +0100

    Clean ImageMetadataApplication.java (main class)

--- src/main/java/com/rspace/rspaceimgmetadata/microservice/ImageMetadataApplication.java
+++ src/main/java/com/rspace/rspaceimgmetadata/microservice/ImageMetadataApplication.java
@@ -2,11 +2,7 @@ package com.rspace.rspaceimgmetadata.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-//@EnableEurekaClient
-@EnableJpaRepositories(basePackages = "com.rspace.rspaceimgmetadata.microservice.repository")
 @SpringBootApplication
 public class ImageMetadataApplication {
 
