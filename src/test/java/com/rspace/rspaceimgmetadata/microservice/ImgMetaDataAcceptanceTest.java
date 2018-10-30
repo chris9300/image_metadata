d4f0587062de2011b2806c71e90d0c95d3e22944
Richard Adams <ra22597@gmail.com>
2018-10-30 09:18:35 +0000

    added status/ endpoint and test setup

---
+++ src/test/java/com/rspace/rspaceimgmetadata/microservice/ImgMetaDataAcceptanceTest.java
@@ -0,0 +1,25 @@
+package com.rspace.rspaceimgmetadata.microservice;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class ImgMetaDataAcceptanceTest {
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void statusTest() {
+		String body = this.restTemplate.getForObject("/status", String.class);
+		assertEquals("OK",body);
+	}
+
+}
