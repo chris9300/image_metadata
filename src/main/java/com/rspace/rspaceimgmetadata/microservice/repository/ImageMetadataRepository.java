6e33cec08a990e7f859df850121ca344f519ce78
Christian Henke <christian.henke@med.uni-goettingen.de>
2018-11-06 11:20:08 +0000

    Set new package after moving Entity-Classes to Model package

--- src/main/java/com/rspace/rspaceimgmetadata/microservice/repository/ImageMetadataRepository.java
+++ src/main/java/com/rspace/rspaceimgmetadata/microservice/repository/ImageMetadataRepository.java
@@ -1,7 +1,7 @@
 package com.rspace.rspaceimgmetadata.microservice.repository;
 
-import com.rspace.rspaceimgmetadata.microservice.service.ImageMetadataEmbeddedKey;
-import com.rspace.rspaceimgmetadata.microservice.service.ImageMetadataEntity;
+import com.rspace.rspaceimgmetadata.microservice.Model.ImageMetadataEmbeddedKey;
+import com.rspace.rspaceimgmetadata.microservice.Model.ImageMetadataEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageMetadataRepository extends JpaRepository<ImageMetadataEntity, ImageMetadataEmbeddedKey> {
